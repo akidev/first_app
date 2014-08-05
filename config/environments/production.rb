484049c3940838964313a7e971aa7a4722ea1bfe
akidev <akidev1101@gmail.com>
2014-08-06 00:59:55 +0900

    support heroku upload

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ FirstApp::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  #  config.active_record.dump_schema_after_migration = false
 end
